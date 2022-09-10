5f82f3bea915c9601668582dbd8bfcbc7dc9819b
zgildo01 <zzaragozagildo@gmail.com>
2022-09-10 14:13:37 -0400

    add formhandler function for rendering user's chosen name on screen

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,30 @@
 /*-------------------------------- Constants --------------------------------*/
+const storyChunks = []; //Fill this out tomorrow, I better see a story in here nerd. I'll kill you. I know where you live. 
 
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-const storyChunks = []; //Fill this out tomorrow, I better see a story in here nerd. I'll kill you. I know where you live. 
+let page = -1; //incrememt every time user clicks on either btn 1, 2 or 3, this will help us decide what index to pull our story from
 
 
 /*------------------------ Cached Element References ------------------------*/
-const storyContent = document.querySelector("#story-content");
-const playerName = document.getElementById("#chosen-name").value;
+const submitBtn = document.querySelector("#submit-btn");
+const chosenName = document.querySelector("#chosen-name")
+const playerNameContainer = document.querySelector("#player-name")
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+submitBtn.addEventListener('click', formHandler);
 
 
 /*-------------------------------- Functions --------------------------------*/
+function formHandler(evt) {
+  evt.preventDefault();
+  const playerName = document.createElement('p');
+  playerName.setAttribute("id", "#user-input-name");
+  playerName.textContent = `Welcome home, ${chosenName.value}.`;
+  playerNameContainer.appendChild(playerName);
+  submitBtn.style.display = "none";
+  chosenName.style.display = "none";
+}
