dda6d13ab500037522ddf3e1d9c2a20606602454
zgildo01 <zzaragozagildo@gmail.com>
2022-09-09 20:55:37 -0400

    add a couple cached element references

--- JS/app.js
+++ JS/app.js
@@ -3,10 +3,12 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+const storyChunks = []; //Fill this out tomorrow, I better see a story in here nerd. I'll kill you. I know where you live. 
 
 
 /*------------------------ Cached Element References ------------------------*/
+const storyContent = document.querySelector("#story-content");
+const playerName = document.getElementById("#chosen-name").value;
 
 
 
