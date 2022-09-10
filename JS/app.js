d77bdd5e821f668b830afab78a126c887693abe2
zgildo01 <zzaragozagildo@gmail.com>
2022-09-10 15:04:41 -0400

    stub up gameinit function, added revealOptions function

--- JS/app.js
+++ JS/app.js
@@ -11,14 +11,22 @@ let page = -1; //incrememt every time user clicks on either btn 1, 2 or 3, this 
 const submitBtn = document.querySelector("#submit-btn");
 const chosenName = document.querySelector("#chosen-name")
 const playerNameContainer = document.querySelector("#player-name")
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.querySelector("#btn-2");
+const btn3 = document.querySelector("#btn-3");
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-submitBtn.addEventListener('click', formHandler);
+submitBtn.addEventListener('click', gameInit);
 
 
 /*-------------------------------- Functions --------------------------------*/
+function gameInit(evt) {
+  formHandler(evt);
+  revealOptions();
+}
+
 function formHandler(evt) {
   evt.preventDefault();
   const playerName = document.createElement('p');
@@ -27,4 +35,10 @@ function formHandler(evt) {
   playerNameContainer.appendChild(playerName);
   submitBtn.style.display = "none";
   chosenName.style.display = "none";
+}
+
+function revealOptions() {
+  btn1.removeAttribute("hidden");
+  btn2.removeAttribute("hidden");
+  btn3.removeAttribute("hidden");
 }
