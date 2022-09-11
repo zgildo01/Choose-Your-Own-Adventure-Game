c58f815518fb8348079bec978fe8d038d9a8b60f
zgildo01 <zzaragozagildo@gmail.com>
2022-09-10 22:04:34 -0400

    change playerName to capitalize first letter, work on changeStory function

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,63 @@
 /*-------------------------------- Constants --------------------------------*/
-const storyChunks = []; //Fill this out tomorrow, I better see a story in here nerd. I'll kill you. I know where you live.
+const storyChunksA = [
+  {
+    storyPath: 'a',
+    storyPart: 0,
+    storySection: "",
+  },
+  {
+    storyPath: 'a',
+    storyPart: 1,
+    storySection: "",
+  },
+  {
+    storyPath: 'a',
+    storyPart: 2,
+    storySection: "",
+  },
+  {
+    storyPath: 'a',
+    storyPart: 3,
+    storySection: "",
+  },
+];
 
+
+const storyChunksB = [
+  {
+    storyPath: 'b',
+    storyPart: 0,
+    storySection: "",
+  },
+  {
+    storyPath: 'b',
+    storyPart: 1,
+    storySection: "",
+  },
+  {
+    storyPath: 'b',
+    storyPart: 2,
+    storySection: "",
+  },
+];
+
+const storyChunksC = [
+  {
+    storyPath: 'c',
+    storyPart: 0,
+    storySection: "",
+  },
+  {
+    storyPath: 'c',
+    storyPart: 1,
+    storySection: "",
+  },
+  {
+    storyPath: 'c',
+    storyPart: 2,
+    storySection: "",
+  },
+];
 
 
 
@@ -15,26 +72,28 @@ const playerNameContainer = document.querySelector("#player-name")
 const btn1 = document.querySelector("#btn-1");
 const btn2 = document.querySelector("#btn-2");
 const btn3 = document.querySelector("#btn-3");
-const btnStoryContent = document.querySelector("#story-content");
+const storyContent = document.querySelector("#story-content");
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 submitBtn.addEventListener('click', gameInit);
+btn1.addEventListener('click', changeStory);
 
 
 /*-------------------------------- Functions --------------------------------*/
 function gameInit(evt) {
   formHandler(evt);
   revealOptions();
-  changeStory();
+  
+  storyContent.textContent = "This is your new home, you've existed here for ages. Do you remember?";
 }
 
 function formHandler(evt) {
   evt.preventDefault();
   const playerName = document.createElement('p');
   playerName.setAttribute("id", "#user-input-name");
-  playerName.textContent = `Welcome home, ${chosenName.value}.`;
+  playerName.textContent = `Welcome home, ${(chosenName.value).substring(0, 1).toUpperCase()+ (chosenName.value).substring(1)}.`;
   playerNameContainer.appendChild(playerName);
   submitBtn.style.display = "none";
   chosenName.style.display = "none";
@@ -47,5 +106,7 @@ function revealOptions() {
 }
 
 function changeStory() {
+  storyContent.textContent = "";
+  page++;
 
 }
