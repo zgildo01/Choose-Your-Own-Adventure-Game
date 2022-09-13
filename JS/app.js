c8986d6d2d2387db5cde4d85d2ee773e2d0ad2ed
zgildo01 <zzaragozagildo@gmail.com>
2022-09-13 11:57:20 -0400

    minor HTML tweaks, finally found way to add playerName into story

--- JS/app.js
+++ JS/app.js
@@ -1,44 +1,4 @@
 /*-------------------------------- Constants --------------------------------*/
-const storyChunks = {
-  "begin": {
-    "content": "You awaken in the greater sea of souls; having just gained consciousness you are but a fraction of what you will become throughout your development. Would you like to begin?",
-    "responses": {
-      "a": "Stay here",
-      "b": "Explore your thoughts",
-      "c": "Begin development",
-    }
-  },
-  //Ending 1, 
-  "1_a": {
-    "content": "And so you decide to stay; numerous aeons pass, but your soul never grows. You remain grounded in the ethereal sea of humanity and will never truly live out your fullest capabilities. This is how you've chosen to meet your end.",
-  },
-  //Good path
-  "1_b": {
-    "content": `You decide to explore the thoughts that perturbed countless of the lives you've lived; solemnly you reflect on one life you've lived in particular, you were a SWAT commander tasked with taking down a cult known for kidnappings and forced indoctrinations; were you civil with them?`,
-    "responses": {
-      "a": "I was just and forthcoming.",
-      "b": "I didn't take them seriously.",
-      "c": "I was brutal in how I ordered the assault.",
-    }
-  },
-  "2_a": {
-    "content": "Their souls were intertwined with yours, they understood you just as much as you understood them. That's how you ended up here. Don't you worry though, my lost child; salvation awaits you. Do you intend to continue your path of righteousness?",
-    "responses": {
-      "a": "I don't think I will.",
-      "b": "I intend to be a good person.",
-    } 
-  },
-
-  //Evil path
-  "1_c": {
-    "content": `Throughout this experience you will encounter many scary and confusing choices, but these will strengthen your resolve. The difficulty of certain choices will tell you more about yourself than your response at times. Are you ready to understand?`,
-    "responses": {
-      "d": "Of course.",
-      "e": "I don't care what this is. I'm not participating.",
-    }
-  }
-
-}
 const letters = /^[A-Za-z]+$/
 
 
@@ -48,13 +8,14 @@ let playerName = '';
 
 /*------------------------ Cached Element References ------------------------*/
 const submitBtn = document.querySelector("#submit-btn");
-const chosenName = document.querySelector("#chosen-name")
+const chosenName = document.querySelector("#chosen-name");
 const playerNameContainer = document.querySelector("#player-name")
 const storyContent = document.querySelector("#story-content");
 const confirmBtn = document.querySelector("#confirm-btn");
 const resetBtn = document.querySelector("#reset-btn");
 const options = document.querySelector("#options");
 const startBtn = document.querySelector("#start");
+const nameQuestion = document.querySelector("#name-question");
 
 /*----------------------------- Event Listeners -----------------------------*/
 startBtn.addEventListener('click', gameInit);
@@ -77,6 +38,7 @@ function gameInit() {
 function formHandler(evt) {
   evt.preventDefault();
   playerName = chosenName.value;
+  nameQuestion.setAttribute("hidden", "true");
   renderPlayerName();
 }
 
@@ -89,7 +51,7 @@ function renderPlayerName() {
     submitBtn.style.display = "none";
     chosenName.style.display = "none";
     startBtn.removeAttribute("hidden");
-    storyContent.innerHTML = `Welcome ${playerName}`
+    storyContent.innerHTML = `Welcome ${playerName}, you will now experience The Engulfment Project; cultists, shriven souls and unholy cosmic entities.`
   } else {
     storyContent.innerHTML = "Your name was less than 20 characters and only contained letters as far as I remember..."
   }
@@ -120,4 +82,47 @@ function renderStory(story) {
 
   storyContent.innerHTML = currentStoryChunk.content;
   options.innerHTML = text;
+}
+
+/*-------------------------------- Story --------------------------------*/
+
+const storyChunks = {
+  "begin": {
+    "content": `You lay in the infinite void, cold and unfeeling; you've achieved such a level of peace that you have never known before, yet, just as you settle in you hear a faint voice. "${chosenName.value}!" Give in?`,
+    "responses": {
+      "a": "Ignore the voice",
+      "b": "Fight to listen",
+      "c": "Begin development",
+    }
+  },
+  //Ending 1, 
+  "1_a": {
+    "content": "Your soul soon leaves your body, having been engulfed by a world-eating cosmic entity. No sooner did you find out what happened to you while you roamed the conscious world than you gave into the calm embrace of non-existance.",
+  },
+  //Good path
+  "1_b": {
+    "content": `You decide to explore the thoughts that perturbed countless of the lives you've lived; solemnly you reflect on one life you've lived in particular, you were a SWAT commander tasked with taking down a cult known for kidnappings and forced indoctrinations; were you civil with them?`,
+    "responses": {
+      "a": "I was just and forthcoming.",
+      "b": "I didn't take them seriously.",
+      "c": "I was brutal in how I ordered the assault.",
+    }
+  },
+  "2_a": {
+    "content": "Their souls were intertwined with yours, they understood you just as much as you understood them. That's how you ended up here. Don't you worry though, my lost child; salvation awaits you. Do you intend to continue your path of righteousness?",
+    "responses": {
+      "a": "I don't think I will.",
+      "b": "I intend to be a good person.",
+    } 
+  },
+
+  //Evil path
+  "1_c": {
+    "content": `Throughout this experience you will encounter many scary and confusing choices, but these will strengthen your resolve. The difficulty of certain choices will tell you more about yourself than your response at times. Are you ready to understand?`,
+    "responses": {
+      "d": "Of course.",
+      "e": "I don't care what this is. I'm not participating.",
+    }
+  }
+
 }
